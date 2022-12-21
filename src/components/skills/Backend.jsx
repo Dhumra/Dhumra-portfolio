8da031717e0845838a1320117f3f77e731a65fcd
Dhumravarna Ambre <103830219+Dhumra@users.noreply.github.com>
2022-12-21 21:58:46 +0530

    Update Backend.jsx

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -46,7 +46,39 @@ const Backend = () => {
 
 
 
+        <div className="skills__group">
 
+           
+
+            <div className="skills__data">
+            <i class='bx bxs-badge-check'></i>
+
+             <div>
+                <h3 className="skills__name">ExpressJs</h3>
+                <span className="skills__level">Intermediate</span>
+             </div>
+
+             
+
+            </div>
+
+            <div className="skills__data">
+            <i class='bx bxs-badge-check'></i>
+
+             <div>
+                <h3 className="skills__name">Java</h3>
+                <span className="skills__level">Intermediate</span>
+             </div>
+
+             
+
+            </div>
+
+           
+
+          
+
+        </div>
 
         
     </div>
